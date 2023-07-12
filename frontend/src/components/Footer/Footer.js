255ab5491713b2a9fa212d8fa2be45c3d956d46f
Murari Kumar <murarikumar42@gmail.com>
2023-07-12 08:15:05 +0530

    FEAT: Add frontend module and initial code for it.

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+      Copyright © 2022 Murari.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
